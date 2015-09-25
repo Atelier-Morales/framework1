f01ddb19b0faddb4d3730c9a027265fb153afc6f
Atelier-Morales <fmorales@student.42.fr>
2015-09-25 17:27:41 +0200

    improved ticket view

--- app/controllers/ticketCtrl.js
+++ app/controllers/ticketCtrl.js
@@ -24,6 +24,11 @@
         function ticketCtrl($rootScope, $scope, $location, $window, $state, $log, $timeout, $stateParams, ticketService, userService, moment) {
             console.log('Ticket section');
             
+            $scope.currentStatus = 'open';
+            $scope.adminFilter = undefined;
+            $scope.myFilter = { status : 'open' };
+            $scope.adminStatus = 'all';
+            
             function fetchCategories() {
                 ticketService.fetchCategories()
                 .success(function(data){
@@ -89,6 +94,28 @@
                 fetchUserTickets($rootScope.userInfo.username);
             })
             
+            $scope.changeTicketStatus = function changeTicketStatus(status) {
+                if (status === $scope.currentStatus)
+                    return;
+                if (status != 'all')
+                    $scope.myFilter = { status : status };
+                else
+                    $scope.myFilter = undefined;
+                console.log($scope.myFilter);
+                $scope.currentStatus = status;
+            }
+            
+            $scope.filterByAdmin = function filterByAdmin(option) {
+                if (status === $scope.adminStatus)
+                    return;
+                if (option != 'all')
+                    $scope.adminFilter = { assignTo : option };
+                else
+                    $scope.adminFilter = undefined;
+                $scope.adminStatus = option;
+                console.log($scope.adminFilter);
+            }
+            
             $scope.fetchId = function() {
                 if ($scope.ticketsCopy === undefined)
                     return;
@@ -118,7 +145,7 @@
                 ticketService.createTicket(title, description, category, author)
                 .success(function(data) {
                     $('#ticketModal').foundation('reveal', 'close');
-                    fetchTickets();
+                    fetchUserTickets($rootScope.userInfo.username);
                 })
                 .error(function(status, data) {
                     console.log(status);
