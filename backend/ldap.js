2e25f4fe8fa0a9f1d6bf49d6251cc1109cc6c1ec
Atelier-Morales <fmorales@student.42.fr>
2015-11-16 19:38:44 +0100

    lol

--- backend/ldap.js
+++ backend/ldap.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 var ldap = require('ldapjs-hotfix');
 var client = ldap.createClient({
     url: 'ldaps://ldap.42.fr'
@@ -23,4 +23,36 @@ function testldap(req, res) {
 }
 
 testldap('fmorales');
+
+uid=fgundlac,ou=july,ou=2013,ou=paris,ou=people,dc=42,dc=fr*/
+
+var passport     = require('passport');
+var LdapStrategy = require('passport-ldapauth');
+
+var getLDAPConfiguration = function(req, callback) {
+  // Fetching things from database or whatever
+    console.log(req);
+  process.nextTick(function() {
+    var opts = {
+      server: {
+        url: 'ldaps://ldap.42.fr',
+        bindDn: 'uid=fmorales,dc=42,dc=fr',
+        searchFilter: '(uid=fmorales)'
+      }
+    };
  
+    callback(null, opts);
+  });
+};
+ 
+passport.use(new LdapStrategy(getLDAPConfiguration, function(user, done) {
+    console.log(user);
+    console.log("fuck");
+    return done(null, user);
+  }
+));
+
+passport.authenticate('ldapauth', {session: false}), function(req, res) {
+    console.log(res);
+  res.send({status: 'ok'});
+};
