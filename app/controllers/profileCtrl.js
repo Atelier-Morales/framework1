e48907ab3048428599160973e101a90b13ced7ae
fernan <fmorales@student.42.fr>
2015-11-24 11:24:00 +0100

    user index

--- app/controllers/profileCtrl.js
+++ app/controllers/profileCtrl.js
@@ -24,6 +24,9 @@
         'authService',
         function UserProfileCtrl($rootScope, $scope, $location, $window, $state, $log, $cookies, $timeout, $translate, userService, authService) {
             
+            $scope.grade = 0;
+            $scope.isLoaded = false;
+
             function fetchAPIinfo(username) {
                 var token = $cookies.get('APIToken');
                 $scope.token = token;
@@ -33,15 +36,30 @@
                     $scope.profile = data;
                     $scope.picture = "https://cdn.intra.42.fr/userprofil/"+data.login+".jpg";
                     $scope.ldap = true;
+                    $scope.isLoaded = true;
                 })
                 .error(function(status, data) {
                     $log.log(status);
                     $log.log(data);
                     $scope.profile = $rootScope.userInfo;
                     $scope.ldap = false;
+                    $scope.isLoaded = true;
                 });
             }
+
+            $scope.greaterThan = function(prop){
+                return function(item){
+                    if ($scope.grade === 'fail')
+                        return item[prop] < 80;
+                    else
+                        return item[prop] > $scope.grade;
+                }
+            }
             
+            $scope.setGrade = function(grade) {
+                $scope.grade = grade;
+            }
+
             $rootScope.$watch('userInfo', function () {
                 if ($rootScope.userInfo === undefined || $rootScope.userInfo === null || $rootScope.userInfo === "")
                     return;
